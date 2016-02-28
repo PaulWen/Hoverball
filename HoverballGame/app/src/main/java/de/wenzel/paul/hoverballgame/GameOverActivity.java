a34197131c64117712b316fbb2a43b99e78f45f6
Paul Wenzel <wenzel.pgb@googlemail.com>
2016-02-28 13:39:19 +0100

    Das eigentliche Hoverball-Game (nicht die Aufgabe) zu Android Studio portiert.

---
+++ HoverballGame/app/src/main/java/de/wenzel/paul/hoverballgame/GameOverActivity.java
@@ -0,0 +1,90 @@
+package de.wenzel.paul.hoverballgame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import de.wenzel.paul.hoverballgame.game.GameActivity;
+
+
+/**
+ * Die Klasse {@link GameOverActivity} ist ein Fenster indem die erreichten Punkte angezeigt werden,
+ * eine neue Spielrunde gestartet werden kann und man zum Start Menü zurück kann.
+ */
+public class GameOverActivity extends Activity implements OnClickListener {
+
+	private Button restartButton;
+	private Button startMenuButton;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// das GameOver-Fenster anzeigen
+		setContentView(R.layout.game_over);
+		
+		// Instanzen von den Buttons aus dem GameOver-Fenster hohlen und den OnClickListener bei ihnen registrieren 
+		restartButton = (Button)findViewById(R.id.restartButton);
+		restartButton.setOnClickListener(this);
+		startMenuButton = (Button)findViewById(R.id.startMenuButton);
+		startMenuButton.setOnClickListener(this);
+		
+		// die Punkte im GameOver-Fenster eintragen
+		((TextView)findViewById(R.id.pointsTextView)).setText("" + getIntent().getExtras().getInt(GameActivity.POINTS));
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// Menü- und Zurück-Button-Ereignisse werden abgefangen
+		if (keyCode == KeyEvent.KEYCODE_MENU) {
+			// die Optionen öffnen
+			Intent intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
+		} else if (keyCode == KeyEvent.KEYCODE_BACK) {
+			// das Start-Menü öffnen
+			finish();
+			Intent intent = new Intent(this, StartActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+		}
+		// sagen, das das Ereignis bearbeitet wurde
+		return true;
+	}
+	
+	@Override
+	public void onClick(View view) {
+		// eine neue Spielrunde starten, wenn der RestartButton gedrückt wird
+		if (view == restartButton) {
+			finish();
+			Intent intent = new Intent(this, GameActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+		// das Start-Menü öffnen, wenn der Start-Menü-Button gedrückt wird
+		} else if (view == startMenuButton) {
+			finish();
+			Intent intent = new Intent(this, StartActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+		}
+	}
+}
---
+++ HoverballGame/app/src/main/java/de/wenzel/paul/hoverballgame/game/GameView.java
@@ -0,0 +1,70 @@
+package de.wenzel.paul.hoverballgame.game;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+
+/**
+ * Die Klasse {@link GameView} ist die View für die KLasse {@link GameActivity}.
+ * Über die Methode "drawGame(...)" kann man die Spielobjekte auf den Bildschirm zeichnen.
+ */
+public class GameView extends SurfaceView {
+ 
+	/** über den {@link SurfaceHolder} kann man sich ein Canvas (Leinwand) hohlen, auf welche man dann zeichnen kann */
+	private SurfaceHolder surfaceHolder;
+	
+	/**
+	 * Der Konstruktor der Klasse {@link GameView}.
+	 * 
+	 * @param context welcher Activity die View angehört, um auf System Daten zugreifen zu dürfen
+	 */
+	public GameView(Context context) {
+		super(context);
+		
+		surfaceHolder = this.getHolder();
+	}
+	
+	/**
+	 * Die Methode zeichnet die übergebenen Objekte in der gewünschten Farbe auf das Canvas(Leinwand).
+	 * 
+	 * @param backgroundColorResourceId die Resource ID der gewünschten Farbe für den Hintergrund
+	 * @param ball der Ball als {@link Rectangle}
+	 * @param ballColorResourceId die Resource ID der gewünschten Farbe für den Ball
+	 * @param barrierList die Liste mit allen Hindernissen die gezeichnet werden sollen
+	 * @param barrierColorResourceId die Resource ID der gewünschten Farbe für die Hindernisse
+	 */
+	public void drawGame(int backgroundColorResourceId, Rectangle ball, int ballColorResourceId, ArrayList<Barrier> barrierList, int barrierColorResourceId ) {
+		if (surfaceHolder.getSurface().isValid()) { 
+			//Canvas hohlen und zum Zeichnen bereitmachen
+			Canvas canvas = surfaceHolder.lockCanvas(); 
+			
+			///////////////Hier wird gezeichnet/////////////////
+			// Hintergrund zeichnen
+			canvas.drawColor(getResources().getColor(backgroundColorResourceId));
+			
+			// Ball zeichnen
+			Paint paint = new Paint();
+			paint.setStyle(Paint.Style.FILL);
+            paint.setColor(getResources().getColor(ballColorResourceId));
+			canvas.drawCircle(ball.getCenterX(), ball.getCenterY(), ball.getHeight() / 2, paint);
+			
+			// Hindernisse zeichnen
+			paint.setColor(getResources().getColor(barrierColorResourceId));
+			for (Barrier barrier : barrierList) {
+				canvas.drawRect(new Rect((int)barrier.getBarrierPart1().getLeft(), (int)barrier.getBarrierPart1().getTop(), (int)barrier.getBarrierPart1().getRight(), (int)barrier.getBarrierPart1().getBottom()), paint);
+				canvas.drawRect(new Rect((int)barrier.getBarrierPart2().getLeft(), (int)barrier.getBarrierPart2().getTop(), (int)barrier.getBarrierPart2().getRight(), (int)barrier.getBarrierPart2().getBottom()), paint);
+			}
+			////////////////////////////////////////////////////
+			
+			surfaceHolder.unlockCanvasAndPost(canvas); //Zeichnen Beenden und zum Anzeigen Freigeben
+		} else {
+			Log.e("SpielView", "Es kann nicht auf das Canvas gezeichnet werden!");
+		}
+	}
+}
